93da3a08b597f4bc5f604a8c41a3f4db4f713b2e
Anshith7S7Kumar <anshith.s@gmail.com>
2025-02-20 13:57:51 +0530

    Home page changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+'use client'
+
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const router = useRouter(); 
   return (
    <>
-   <h1>Home page</h1>
+   <h1 className="text-3xl text-center font-sans font-bold p-4 text-red-400">Welcome to YT Playlist retriever!</h1>
+   <div className='flex justify-center items-center'>
+   <button onClick={() => router.push('/components/signIn')} className="border border-red-300 p-4">Redirect to SignIn</button>
+   </div>
    </>
   );
 }
