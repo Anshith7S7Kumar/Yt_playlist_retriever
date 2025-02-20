d1e65784dfbe47643a8bef5b7288da0225f7baed
Anshith7S7Kumar <anshith.s@gmail.com>
2025-02-20 15:05:26 +0530

    router added on signIn page

--- app/components/signIn/page.tsx
+++ app/components/signIn/page.tsx
@@ -1,12 +1,16 @@
 
 import { signIn } from "@/auth"
 import Image from "next/image";
+import { useRouter  } from "next/router";
 
 export default function SignIn() {
+
+  const router = useRouter(); 
 
   const handleSignIn = async() => {
     "use server"
     await signIn("google", {redirectTo:process.env.AUTH_REDIRECT_URI}); 
+    router.push('/components/playlists')
   }
   return (
     <form
